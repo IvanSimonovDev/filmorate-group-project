7491a015d43d55af0161112554f1755e2ad160f3
Ivan Simonov <Inculto@yandex.ru>
2024-11-11 22:19:53 +0300

    Fix после ревью 2

--- src/main/java/ru/yandex/practicum/filmorate/repository/inDatabase/JdbcReviewsLikesDislikesRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/inDatabase/JdbcReviewsLikesDislikesRepository.java
@@ -1,57 +1,70 @@
 package ru.yandex.practicum.filmorate.repository.inDatabase;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.model.Review;
 import ru.yandex.practicum.filmorate.repository.ReviewsLikesDislikesRepository;
 
+import java.util.Map;
+
 @Repository
-@RequiredArgsConstructor
-public class JdbcReviewsLikesDislikesRepository implements ReviewsLikesDislikesRepository {
-    private final JdbcTemplate jdbc;
+public class JdbcReviewsLikesDislikesRepository extends JdbcBaseRepository<Review> implements ReviewsLikesDislikesRepository {
+
     private static final String insertSql = """
             INSERT INTO reviews_likes_dislikes (reviewId, userId, val)
-            VALUES (?, ?, ?);
+            VALUES (:reviewId, :userId, :val);
             """;
 
     private static final String deleteSql = """
             DELETE FROM reviews_likes_dislikes
-            WHERE (reviewId = ?)
-            AND (userID = ?)
-            AND (val = ?);
+            WHERE (reviewId = :reviewId)
+            AND (userID = :userId)
+            AND (val = :val);
             """;
 
+    public JdbcReviewsLikesDislikesRepository(NamedParameterJdbcOperations jdbc) {
+        super(jdbc, null);
+    }
+
     public void likeReview(long reviewId, long userId) {
-        jdbc.update(insertSql, reviewId, userId, 1);
+        insert(insertSql, createParamsMap(reviewId, userId, 1));
     }
 
     public void dislikeReview(long reviewId, long userId) {
-        jdbc.update(insertSql, reviewId, userId, -1);
+        insert(insertSql, createParamsMap(reviewId, userId, -1));
     }
 
     public void deleteLikeOnReview(long reviewId, long userId) {
-        jdbc.update(deleteSql, reviewId, userId, 1);
+        delete(deleteSql, createParamsMap(reviewId, userId, 1));
     }
 
     public void deleteDislikeOnReview(long reviewId, long userId) {
-        jdbc.update(deleteSql, reviewId, userId, -1);
+        delete(deleteSql, createParamsMap(reviewId, userId, -1));
     }
 
     public long getReviewRating(long reviewId) {
         String getCountSql = """
                 SELECT COUNT(val)
                 FROM reviews_likes_dislikes
-                WHERE reviewId = ?;
+                WHERE reviewId = :reviewId;
                 """;
         String getRatingSql = """
                 SELECT SUM(val)
                 FROM reviews_likes_dislikes
-                WHERE reviewId = ?
+                WHERE reviewId = :reviewId
                 GROUP BY reviewId;
                 """;
 
-        Long count = jdbc.queryForObject(getCountSql, Long.class, reviewId);
+        Map<String, Long> params = Map.of("reviewId", reviewId);
 
-        return (count == 0 ? 0 : jdbc.queryForObject(getRatingSql, Long.class, reviewId));
+        Long count = jdbc.queryForObject(getCountSql, params, Long.class);
+
+        return (count == 0 ? 0 : jdbc.queryForObject(getRatingSql, params, Long.class));
+    }
+
+    private Map<String, Long> createParamsMap(long reviewId, long userId, long val) {
+        return Map.of("reviewId", reviewId,
+                "userId", userId,
+                "val", val);
     }
 }
