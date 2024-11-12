ed78b1acbdcc0ae16351e013912bee5c6928c5a6
Karon Stout <70218052+semigris@users.noreply.github.com>
2024-11-12 22:07:52 +0300

    Функциональность «Лента событий»

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Event.java
@@ -0,0 +1,23 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+import ru.yandex.practicum.filmorate.enums.*;
+
+@Data
+@Builder
+public class Event {
+    Long id;
+    @NotNull(message = "Пользователь должен быть указан")
+    @NotNull(message = "id должен быть указан")
+    Long userId;
+    @NotNull(message = "Временная метка не может быть пустой")
+    Long timestamp;
+    @NotNull(message = "Тип события должен быть указан. Укажите LIKE, REVIEW или FRIEND")
+    EventType eventType;
+    @NotNull(message = "Тип операции должен быть указан. Укажите ADD,REMOVE или UPDATE")
+    Operation operation;
+    @NotNull(message = "id сущности, с которой произошло событие, не может быть пустым")
+    Long entityId;
+}
